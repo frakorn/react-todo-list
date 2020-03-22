24ba8cdcbefecabb8790f9dfb67002da61ac5247
francesco cini <frakorn@gmail.com>
2020-03-22 15:20:15 +0100

    moved component sortable in another folder

--- src/components/edit-list/edit-list.js
+++ src/components/edit-list/edit-list.js
@@ -2,45 +2,8 @@ import React, { Component } from "react";
 import list from "../../storage/list.json"
 import '../edit-list/edit-list.scss';
 import { toast } from 'react-toastify';
-import { SortableContainer, SortableElement } from 'react-sortable-hoc';
 import arrayMove from 'array-move';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowsAlt } from '@fortawesome/free-solid-svg-icons'
-
-const SortableItem = SortableElement(({ list, editItem, removeItem }) => {
-     return (
-        <li key={list.id} className="list-group-item" data-toggle="modal" data-target="#editModal">
-            <div className="input-group mb-3 drag">
-                <div className="input-group-prepend">
-                    <span className="input-group-text">
-                        <FontAwesomeIcon icon={faArrowsAlt} />
-                    </span>
-                </div>
-                <div className="custom-file">
-                    <input onChange={editItem.bind(this, list.id)} value={list.value} className="form-control" type="text" placeholder=".form-control-lg"></input>
-                </div>
-                <div className="input-group-prepend">
-                    <button onClick={removeItem.bind(this, list.id)} className="delete btn btn-warning">Delete</button>
-                </div>
-            </div>
-        </li>
-    )
-});
-
-const SortableList = SortableContainer(({ items, removeItem, editItem }) => {
-    return (
-        <ol className="list-group list_of_items">
-            {items.map((list,i) => (
-                <SortableItem editItem={editItem}
-                    removeItem={removeItem}
-                    editItem={editItem}
-                    key={`item-${list.id}`}
-                    index={i}
-                    list={list} />
-            ))}
-        </ol>
-    );
-});
+import Sortable from '../sortable/sortable';
 
 export default class EditList extends Component {
     constructor(props) {
@@ -91,7 +54,7 @@ export default class EditList extends Component {
                         <button onClick={this.save} type="button" className="btn btn-success">Save</button>
                     </div>
                 </div>
-                <SortableList removeItem={this.removeItem}
+                <Sortable removeItem={this.removeItem}
                     editItem={this.editItem}
                     items={this.state.list}
                     onSortEnd={this.onSortEnd} />
---
+++ src/components/sortable/sortable.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowsAlt } from '@fortawesome/free-solid-svg-icons'
+
+const SortableItem = SortableElement(({ list, editItem, removeItem }) => {
+     return (
+        <li key={list.id} className="list-group-item" data-toggle="modal" data-target="#editModal">
+            <div className="input-group mb-3 drag">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">
+                        <FontAwesomeIcon icon={faArrowsAlt} />
+                    </span>
+                </div>
+                <div className="custom-file">
+                    <input onChange={editItem.bind(this, list.id)} value={list.value} className="form-control" type="text" placeholder=".form-control-lg"></input>
+                </div>
+                <div className="input-group-prepend">
+                    <button onClick={removeItem.bind(this, list.id)} className="delete btn btn-warning">Delete</button>
+                </div>
+            </div>
+        </li>
+    )
+});
+
+const SortableList = SortableContainer(({ items, removeItem, editItem }) => {
+    return (
+        <ol className="list-group list_of_items">
+            {items.map((list,i) => (
+                <SortableItem editItem={editItem}
+                    removeItem={removeItem}
+                    editItem={editItem}
+                    key={`item-${list.id}`}
+                    index={i}
+                    list={list} />
+            ))}
+        </ol>
+    );
+});
+
+export default class Sortable extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <SortableList items={this.props.items}
+                            removeItem={this.props.removeItem}
+                            editItem={this.props.editItem}
+                            onSortEnd={this.props.onSortEnd} />;
+    }
+}
